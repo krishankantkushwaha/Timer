bd84d0c8d53734101da93e136abb8530fef07908
krishankantkushwaha <krishankantkushwaha719@gmail.com>
2023-12-04 17:58:26 +0530

    first commit

---
+++ app/src/main/java/com/example/timer/ss.java
@@ -0,0 +1,21 @@
+package com.example.timer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class ss extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ss);
+        new Handler().postDelayed(() -> {
+            Intent i= new Intent(ss.this, MainActivity.class);
+            startActivity(i);
+            finish();
+        },2000);
+    }
+}
